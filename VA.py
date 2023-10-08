1c19927abd708e2dc65f16a22a668cefc0986ca6
CipherKrypt <SS4334@live.mdx.ac.uk>
2023-10-08 22:39:15 +0400

    Fine tuning multiple modules

--- VA.py
+++ VA.py
@@ -1,5 +1,6 @@
 from module_config import *
 from Master import *
+import logging
 
 class VA:
     def __init__(self, name):
@@ -9,24 +10,36 @@ class VA:
         self.name = name
         self.init = False
         self.master = None
+        self.logger = self.set_logger()
+
         self.speak(self.greeting())
+
+    def set_logger(self):
+        logger = logging.getLogger("VA")
+        logger.setLevel(logging.DEBUG)  # Set log level
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        file_handler = logging.FileHandler(f'{self.name}Logger.log')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        return logger
 
     def greeting(self):
         import datetime
         hour = int(datetime.datetime.now().hour)
         if hour>= 0 and hour<12:
-            s =  "Good Morning"
+            s =  "Good Morning!"
     
         elif hour>= 12 and hour<18:
-            s = "Good Afternoon"   
+            s = "Good Afternoon!"   
     
         else:
-            s = "Good Evening"
+            s = "Good Evening!"
 
-        return s + f'I am {self.name}. At your service'
+        return s + f' I am {self.name}. At your service'
     
-# FUnction to take in text and convert to Audio
-def speak(self,text):
-    self.engine.say(text)
-    self.engine.runAndWait()
-    
+    # FUnction to take in text and convert to Audio
+    def speak(self,text):
+        self.engine.say(text)
+        self.logger.debug(f'Saying : {text}')
+        self.engine.runAndWait()
+        
--- VA_main.py
+++ VA_main.py
@@ -1,12 +1,80 @@
-from module_config import *
+from VA import *
+from pickle import Pickler, Unpickler
+import speech_recognition as sr
+from time import time
+import logging 
+import threading
+# Backlog to track unsaved changes
+BackLog = []
 
-# Text-Speech engine config
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[1].id)
+# Logger for LISTENER
+logger01 = logging.getLogger('listener')
+logger01.setLevel(logging.DEBUG)  # Set log level
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+file_handler = logging.FileHandler('ListenerLogger.log')
+file_handler.setFormatter(formatter)
+logger01.addHandler(file_handler)
 
-# FUnction to take in text and convert to Audio
-def speak(text):
-    engine.say(text)
-    engine.runAndWait()
+# Logger for VA
+logger02 = logging.getLogger('Assistant')
+logger02.setLevel(logging.WARNING)  # Set log level
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+file_handler = logging.FileHandler('AssistantLogger.log')
+file_handler.setFormatter(formatter)
+logger02.addHandler(file_handler)
 
+# Logger for Serialization
+logger03 = logging.getLogger('Serialization')
+logger03.setLevel(logging.WARNING)  # Set log level
+formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+file_handler = logging.FileHandler('SerializationLogger.log')
+file_handler.setFormatter(formatter)
+logger03.addHandler(file_handler)
+
+r = sr.Recognizer()
+# Listens for user inputs
+def listen():
+    
+    with sr.Microphone() as source:
+         
+        logger01.debug("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+  
+    try:
+        logger01.debug("Recognizing...")    
+        query = r.recognize_google(audio, language ='en-in')
+        logger01.debug(f"User said: {query}")
+  
+    except Exception as e:
+        logger01.warning(f'Error {e}')    
+        print(f"Unable to Recognize your voice. {e}")  
+        return "None"
+     
+    return query
+
+# checks for a word within a reply
+def check_for(reply: str, search:str) -> bool: 
+    words = reply.split()
+    for word in words:
+        if word.lower() == search.lower():
+            return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    while True:
+        print("Say hello followed by Virtual Assitant's name")
+        reply = listen()
+        words = reply.split()
+        for word in words:
+            if word.lower() != "hello":
+                name = word
+                break
+        print(f"Your Assistant's name is {name.title()}. Is this correct?")
+        command = listen()
+        if check_for(command, "yes"):
+            jarvis = VA(name.title())
+            break
+        else:
+            continue
